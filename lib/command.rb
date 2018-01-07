684b1c6c1dc0aaec56e15a33b9506dc20bfb40f9
Razvan Horeanga <razvan.horeanga@gmail.com>
2018-01-06 18:24:21 -0600

    refactored & added functionality for command class

--- lib/command.rb
+++ lib/command.rb
@@ -1,7 +1,24 @@
 class Command
 
-  def initialize(command_text)
+  def initialize(todos,command_text)
+    @todos = todos
+    dispatch(command_text)
+  end
 
+  private
+
+  def command_name(command_text)
+    command_text.partition(" ")[0]
+  end
+
+  def rest_of_command(command_text)
+    command_text.partition(" ")[2]
+  end
+
+  def dispatch(command_text)
+    if command_name(command_text) == "create"
+      @todos.create(rest_of_command(command_text))
+    end
   end
 
 end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,11 +1,22 @@
 require "Command"
 
 describe Command do
+
+  let(:todos) {double(:todos)}
+
   describe "::new" do
 
-    subject {Command.new("new get milk")}
+    subject {Command.new(todos,"new get milk")}
     it "is instantiated with a command string" do
       expect(subject).to be_a Command
+    end
+  end
+
+  describe "running a command" do
+
+    it "can run the create command" do
+      expect(todos).to receive(:create).with("get milk")
+      subject = Command.new(todos,"create get milk")
     end
   end
 end
