7abf730005b6a58966404c879a03faa48d1a5fa2
Razvan Horeanga <razvan.horeanga@gmail.com>
2018-01-07 11:59:50 -0600

    refactored the cli output

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,11 +2,14 @@ require_relative './todos'
 require_relative './todo'
 require_relative './command'
 
-puts 'Available commands: create, list, complete(index), exit'
+instructions = 'Available commands: create, list, complete(index), exit'
+puts instructions,""
 
 todos = Todos.new(Todo)
 
 until (command_text = gets.chomp) =~ /(?:ex|qu)it/i
+  system "clear"
+  puts instructions,""
   Command.new(todos,command_text)
 end
 
